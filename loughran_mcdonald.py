cc56d89631debd61cedfe114f52ffe145eaf150a
alexwang36 <awang3648@gmail.com>
2024-04-06 22:09:06 -0400

    split transcripts into question answer pairs

--- loughran_mcdonald.py
+++ loughran_mcdonald.py
@@ -485,25 +485,82 @@ def tone_count_with_negation_check(dict, article):
 
 
 folder_path = 'C:/Users/awang/Downloads/Earnings-Calls-NLP-main/Earnings-Calls-NLP-main/transcripts/sandp500'
+#folder_path = 'C:/Users/awang/Downloads/fake'
 transcripts_results = []
 
 
+
+questions = []
+answers = []
+transcript = []
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".txt"):
         file_path = os.path.join(folder_path, filename)
 
         with open(file_path, "r") as file:
-            text = file.read()
-            transcript_nlp = tone_count_with_negation_check(lmdict, text)
+            text = file.readlines()
+
+            operator_flag = False
+            question_flag = False
+            question_asker = ""
+            question = ""
+            
+            for i in range(1, len(text)):
+                line = text[i].strip()
+
+                if line.strip() == "Operator" and "question" in text[i+1].strip():
+                    operator_flag = True
+
+                if "?" in line and operator_flag:
+                    question_asker = text[i-1].strip().split()
+                    print(question_asker)
+                    if len(question_asker) > 1:
+                        if len(question_asker[1]) > 1:
+                            if question_asker[1][0].isupper or question_asker[1][1] == '.':
+                                question_flag = True
+                                operator_flag = False
+                                #print(question_asker)
+                                question = line.strip()
+                
+                words = line.split()
+                if question_flag and len(words) > 1 and len(question_asker) > 1:
+                    if len(words[1]) > 1 and len(question_asker[1]) > 1:
+                        if len(words) < 10 and (words[1][0].isupper() or (len(words[1]) == 2 and words[1][1] == ".")) and (words[0] != question_asker[0] and words[1] != question_asker[1]):
+                            question_flag = False
+                            questions.append(question)
+                            answers.append(text[i+1].strip())
+                            transcript.append(filename)
+
+            #uncomment this later:
+            #transcript_nlp = tone_count_with_negation_check(lmdict, text)
+
+
             #transcript_result = [filename] + transcript_nlp
             #transcripts_results.append(transcript_result)
-            transcripts_results.append(transcript_nlp)
 
-df = pd.DataFrame(transcripts_results)
-pd.set_option('display.max_colwidth', None)
+            #uncomment this later:
+            #transcripts_results.append(transcript_nlp)
+
+#df = pd.DataFrame(transcripts_results)
+#pd.set_option('display.max_colwidth', None)
 #df.columns = ['name transcript', 'word_count', 'pos_words', 'neg_words']
-df.columns = ['word_count', 'pos_words', 'neg_words']
+#df.columns = ['word_count', 'pos_words', 'neg_words']
 
-print(df)
+print(len(questions))
+print(len(answers))
+
+for i in range(1, len(answers)):
+    print("question: ")
+    print(questions[i])
+    print("\n")
+    print("answer: ")
+    print(answers[i])
+    print("\n")
+
+print(answers)
+print(len(answers))
+df = pd.DataFrame({'Questions': questions, 'Answers': answers, 'Transcript File': transcript})
+
+df.to_csv('C:/Users/awang/theil_fnp-20/question-answer.csv')
 
